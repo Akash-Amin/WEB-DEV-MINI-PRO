945803dcefd3d22295f46eea9e1c93f26e621456
Akash-Amin <83532164+Akash-Amin@users.noreply.github.com>
2021-07-06 20:15:06 +0530

    Add files via upload

---
+++ StudyMaterials/More.js
@@ -0,0 +1,66 @@
+import React from "react";
+import'./StudyMaterials.css';
+import './StudyMaterials.css'
+import dc1 from "./../ECE/dc1.jpg"
+
+const StudyMaterials=()=>{
+    return(<> 
+ 
+    <div className="sm-main">
+    <div>
+    <div className='title'>
+    <label >DIGITAL COMMUNICATION</label>
+    </div>
+    <div className='more-main-div'>
+    <img src={dc1} className='image'/>
+    <div className="more-text-box">
+    <h2 className="dc-main-head">WHY DIGITAL COMMUNICATION?</h2>
+    
+    <p className="dc-paragraph">lorem dsn dsn cbcb cjjdddddd ddddddd ddddddddd dddddddd dddddddd dddddd dddd sssss s s sssss snc cbcx</p>
+    </div>
+    </div>
+    
+    <h2 className="dc-head">CHAPTER 1</h2>
+
+    <div className='more-container'>
+
+    
+    
+    <iframe className="video video1"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/sdqL1_hBMpM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    <iframe className="video video1"  src="https://www.youtube.com/embed/W4OfUjgCC7o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/tScjSC-S-5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/52P6d6z3Ox8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/nzRhhOH1aOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    
+    </div>
+
+    
+    <h2 className="dc-head">CHAPTER 2</h2>
+
+    <div className='more-container'>
+
+    
+    
+    <iframe className="video video1"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/MdShEV8CpNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/sdqL1_hBMpM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    <iframe className="video video1"  src="https://www.youtube.com/embed/W4OfUjgCC7o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/tScjSC-S-5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/52P6d6z3Ox8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="video"  src="https://www.youtube.com/embed/nzRhhOH1aOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    
+    </div>
+    </div>
+    </div>
+    </>);
+};
+
+export default StudyMaterials;
