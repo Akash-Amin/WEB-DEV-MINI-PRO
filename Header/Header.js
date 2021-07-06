560ba438b296d4557a310baa62038606e8776a9d
Akash-Amin <83532164+Akash-Amin@users.noreply.github.com>
2021-07-06 19:44:53 +0530

    Add files via upload

---
+++ Header/Header.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { BrowserRouter,Switch,Route,Redirect}   from "react-router-dom";
+import AboutUs from "../../Container/AboutUs/AboutUs";
+import StudyMaterials from "../../Container/StudyMaterials/StudyMaterials";
+import HeaderPath from "./HeaderPath";
+import Home from "../../Container/Home/Home"
+import More from "../../Container/StudyMaterials/More";
+
+
+
+
+
+const Header=()=>{
+return(
+    <>
+     
+    <BrowserRouter>
+    <HeaderPath/>
+    
+   
+    <Switch>
+      <Route  exact path="/" component={Home} />
+      <Route exact path="/studymaterials" component={StudyMaterials} />
+      <Route exact path="/aboutus" component={AboutUs} />
+      <Route exact path="/more" component={More} />
+      
+
+    
+    </Switch>
+    </BrowserRouter>
+    </>);
+}
+
+export default Header;
